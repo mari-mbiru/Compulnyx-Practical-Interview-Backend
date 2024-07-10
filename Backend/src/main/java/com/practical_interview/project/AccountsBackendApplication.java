40226641b2ed379310efc553d544211d1d17790d
mari-mbiru <marimbiru@gmail.com>
2024-07-10 20:25:10 +0300

    Backend project initialize

---
+++ Backend/src/main/java/com/practical_interview/project/AccountsBackendApplication.java
@@ -0,0 +1,14 @@
+package com.practical_interview.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsBackendApplication.class, args);
+	}
+
+	
+}
