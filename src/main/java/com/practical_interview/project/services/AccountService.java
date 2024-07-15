6193534916cc27ee49f0606fe302712abdaac179
mari-mbiru <marimbiru@gmail.com>
2024-07-15 08:36:55 +0300

    - fix null pointer error when accounts service is creating transactions

--- src/main/java/com/practical_interview/project/services/AccountService.java
+++ src/main/java/com/practical_interview/project/services/AccountService.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.UUID;
 
 @Service
@@ -88,6 +89,7 @@ public class AccountService {
                 .account(account)
                 .dateCreated(LocalDateTime.now())
                 .transferId(transferID)
+                .relatedTransactions(new HashSet<>())
                 .build();
     }
 
